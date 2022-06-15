65e666854d0e870ce5ac82537bace5028ba90650
minhleduc0210 <bastreet124@gmail.com>
2022-06-15 11:58:47 +0700

    Second commit on Learning React

--- Learning React/my-todo-app/src/components/TodoApp.js
+++ Learning React/my-todo-app/src/components/TodoApp.js
@@ -2,15 +2,77 @@
 
 import React from "react";
 import Header from "./layout/Header";
-// import { render } from "react-dom";
+import Todos from "./Todos";
+import AddTodo from "./AddTodo";
+
+import {v4 as uuidv4} from "uuid"; 
 
 class TodoApp extends React.Component{
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+                {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
+
+    handleCheckboxChange = id => {
+        //console.log("clicked"); 
+        //console.log("clicked on checkbox with id = " + id);
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if(todo.id === id){
+                    todo.completed = !todo.completed; 
+                }
+                return todo; 
+            })
+        });
+    };
+
+    deleteTodo = id => {
+        //console.log("deleted", id); 
+        this.setState({
+            todos:[
+                ...this.state.todos.filter(todo => {
+                    return todo.id !== id; 
+                })
+            ]
+        });
+    };
+
+    addTodo = title => {
+        //console.log(title); 
+        const newTodo = {
+            id: uuidv4(), 
+            title: title, 
+            completed: false
+        }; 
+        this.setState({
+            todos:[...this.state.todos, newTodo]
+        }); 
+    }
+
     render(){
         return(
-            <div>
-                {/* <h1>Hello from Create React App</h1>
-                <p>I am in a React Component!</p> */}
+            <div className="container">
                 <Header/>
+                <AddTodo addTodo={this.addTodo} />
+                <Todos todos = {this.state.todos} 
+                        handleChange={this.handleCheckboxChange}
+                        deleteTodo={this.deleteTodo}
+                />
             </div>
         ); 
     }
