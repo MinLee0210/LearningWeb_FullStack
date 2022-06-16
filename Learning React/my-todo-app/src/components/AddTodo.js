be1793decb312ef08993497845087293fdcffdeb
minhleduc0210 <bastreet124@gmail.com>
2022-06-16 11:13:28 +0700

    Change from Class Component to Functional Component

--- Learning React/my-todo-app/src/components/AddTodo.js
+++ Learning React/my-todo-app/src/components/AddTodo.js
@@ -1,42 +1,73 @@
-import React from "react"; 
+import React, { useState } from "react"; 
 
-class AddTodo extends React.Component{
+// class AddTodo extends React.Component{
 
-    state = {
-        title: ""
-    }; 
+//     state = {
+//         title: ""
+//     }; 
 
-    onInputChange = e => {
-        this.setState({
-            title: e.target.value
-        });
-    };
+//     onInputChange = e => {
+//         this.setState({
+//             title: e.target.value
+//         });
+//     };
 
-    addTodo = e => {
+//     addTodo = e => {
+//         e.preventDefault(); 
+//         //console.log(this.state.title);
+//         this.props.addTodo(this.state.title); 
+//         this.setState({
+//             title: ""
+//         });
+//     }
+
+//     render(){
+//         return(
+//             <form className="form-container" onSubmit={this.addTodo}>
+//                 <input type="text"
+//                         placeholder="Add Todo ..."
+//                         className="input-text"
+//                         value={this.state.title}
+//                         onChange={this.onInputChange}
+//                 />
+//                 <input type="submit"
+//                         value="Submit"
+//                         className="input-sunmit"
+//                 />
+//             </form>
+//         ); 
+//     }
+// }
+
+function AddTodo (props) {
+    const [title, setTitle] = useState(""); 
+    
+    const onInputChange = e => {
+        setTitle(e.target.value); 
+    }
+
+    const addTodo = e => {
         e.preventDefault(); 
-        //console.log(this.state.title);
-        this.props.addTodo(this.state.title); 
-        this.setState({
-            title: ""
-        });
+        props.addTodo(title); 
+        setTitle(""); 
     }
 
-    render(){
-        return(
-            <form className="form-container" onSubmit={this.addTodo}>
-                <input type="text"
-                        placeholder="Add Todo ..."
-                        className="input-text"
-                        value={this.state.title}
-                        onChange={this.onInputChange}
-                />
-                <input type="submit"
-                        value="Submit"
-                        className="input-sunmit"
-                />
-            </form>
-        ); 
-    }
+    return(
+        <form className="form-container" onSubmit={addTodo}>
+            <input
+                type="text"
+                placeholder="Add Todo..."
+                className="input-text"
+                value={title}
+                onChange={onInputChange}
+            />
+            <input
+                type="submit"
+                value="Submit"
+                className="input-submit"
+            />
+
+        </form>
+    )
 }
-
 export default AddTodo; 
--- Learning React/my-todo-app/src/components/TodoApp.js
+++ Learning React/my-todo-app/src/components/TodoApp.js
@@ -1,38 +1,42 @@
 /* eslint-disable */
 
 import React from "react";
+import { useState, useEffect } from "react";
+
 import Header from "./layout/Header";
 import Todos from "./Todos";
 import AddTodo from "./AddTodo";
 
 import {v4 as uuidv4} from "uuid"; 
+import axios from "axios";
 
-class TodoApp extends React.Component{
-    state = {
-        todos: [
-            {
-                id: 1,
-                title: "Setup development environment",
-                completed: true
-            },
-            {
-                id: 2,
-                title: "Develop website and add content",
-                completed: false
-            },
-                {
-                id: 3,
-                title: "Deploy to live server",
-                completed: false
-            }
-        ]
-    };
+// class TodoApp extends React.Component{
+function TodoApp() {
+    // state = {
+    //     todos: [
+    //     ]
+    // };
+    const [state, setState] = useState({
+        todos: []
+    })
 
-    handleCheckboxChange = id => {
+    // handleCheckboxChange = id => {
+    //     //console.log("clicked"); 
+    //     //console.log("clicked on checkbox with id = " + id);
+    //     this.setState({
+    //         todos: this.state.todos.map(todo => {
+    //             if(todo.id === id){
+    //                 todo.completed = !todo.completed; 
+    //             }
+    //             return todo; 
+    //         })
+    //     });
+    // };
+    const handleCheckboxChange = id => {
         //console.log("clicked"); 
         //console.log("clicked on checkbox with id = " + id);
-        this.setState({
-            todos: this.state.todos.map(todo => {
+        setState({
+            todos: state.todos.map(todo => {
                 if(todo.id === id){
                     todo.completed = !todo.completed; 
                 }
@@ -41,41 +45,113 @@ class TodoApp extends React.Component{
         });
     };
 
-    deleteTodo = id => {
-        //console.log("deleted", id); 
-        this.setState({
-            todos:[
-                ...this.state.todos.filter(todo => {
-                    return todo.id !== id; 
+    // deleteTodo = id => {
+    //     //console.log("deleted", id); 
+    //     // this.setState({
+    //     //     todos:[
+    //     //         ...this.state.todos.filter(todo => {
+    //     //             return todo.id !== id; 
+    //     //         })
+    //     //     ]
+    //     // });
+    //     axios.delete("https://jsonplaceholder.typicode.com/todos/${id}")
+    //         .then(response => this.setState({
+    //             todos:[
+    //                 ...this.state.todos.filter(todo => {
+    //                     return todo.id !== id; 
+    //                 })
+    //             ]
+    //         }))
+    // };
+    const deleteTodo = id => {
+        axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+            .then(reponse => setState({
+        todos: [
+            ...state.todos.filter(todo => {
+                return todo.id !== id;
                 })
             ]
-        });
+        }))
     };
 
-    addTodo = title => {
-        //console.log(title); 
-        const newTodo = {
-            id: uuidv4(), 
-            title: title, 
+    // addTodo = title => {
+    //     //console.log(title); 
+    //     // const newTodo = {
+    //     //     id: uuidv4(), 
+    //     //     title: title, 
+    //     //     completed: false
+    //     // }; 
+    //     // this.setState({
+    //     //     todos:[...this.state.todos, newTodo]
+    //     // }); 
+    //     const todoData = {
+    //         title: title, 
+    //         completed: false
+    //     }
+    //     axios.post("https://jsonplaceholder.typicode.com/todos", todoData)
+    //         .then(response => {
+    //             console.log(response.data); 
+    //             this.setState({
+    //                 todos: [...this.state.todos, response.data]
+    //             })
+    //         })
+    // };
+    const addTodo = title => {
+        const todoData = {
+            title: title,
             completed: false
-        }; 
-        this.setState({
-            todos:[...this.state.todos, newTodo]
-        }); 
-    }
+        }
+        axios.post("https://jsonplaceholder.typicode.com/todos", todoData)
+            .then(response => {
+                console.log(response.data)
+                setState({
+                    todos: [...state.todos, response.data]
+                    })
+            });
+    };
 
-    render(){
-        return(
-            <div className="container">
-                <Header/>
-                <AddTodo addTodo={this.addTodo} />
-                <Todos todos = {this.state.todos} 
-                        handleChange={this.handleCheckboxChange}
-                        deleteTodo={this.deleteTodo}
-                />
-            </div>
-        ); 
-    }
+    // componentDidMount(){
+    //     const config = {
+    //         params: {
+    //             _limit : 5
+    //         }
+    //     }
+    //     axios.get("https://jsonplaceholder.typicode.com/todos", config)
+    //          .then(response => this.setState({todos: response.data})); 
+    // };
+    useEffect(() => {
+        const config = {
+            params: {
+                _limit : 5
+            }
+        }
+        axios.get("https://jsonplaceholder.typicode.com/todos", config)
+             .then(response => setState({todos: response.data})); 
+    }, []);
+    
+    // render(){
+    //     return(
+    //         <div className="container">
+    //             <Header/>
+    //             <AddTodo addTodo={this.addTodo} />
+    //             <Todos todos = {this.state.todos} 
+    //                     handleChange={this.handleCheckboxChange}
+    //                     deleteTodo={this.deleteTodo}
+    //             />
+    //         </div>
+    //     ); 
+    // }
+    return(
+        <div className="container">
+            <Header/>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos = {state.todos} 
+                    handleChange={handleCheckboxChange}
+                    deleteTodo={deleteTodo}
+            />
+        </div>
+    ); 
+        
 }
 
 export default TodoApp; 
